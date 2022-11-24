357f3745a3b7d7efa06d35094030effc362675c1
Morvanmc <morvanmc@outlook.com>
2022-11-23 21:04:19 -0300

    Setup NodeJS + TS project

---
+++ src/util/module-alias.ts
@@ -0,0 +1,10 @@
+// Arquivo para facilitar a importação de arquivos dentro do nosso projeto
+import * as path from 'path';
+import moduleAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../..');
+
+moduleAlias.addAliases({
+    '@src': path.join(files, 'src'),
+    '@test': path.join(files, 'test')
+});
